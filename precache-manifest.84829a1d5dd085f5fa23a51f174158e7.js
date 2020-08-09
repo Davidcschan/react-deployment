218e9e0078cd2e7885ee1b93f0c008053816289d
davidcschan <cs.chan1999@gmail.com>
2020-08-09 17:14:55 +0800

    Updates

---
+++ precache-manifest.84829a1d5dd085f5fa23a51f174158e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411af1e3ddd834131d8ea3caaa94ad0a",
+    "url": "/react-deployment/index.html"
+  },
+  {
+    "revision": "9fa1d5938fe52b0e817a",
+    "url": "/react-deployment/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/react-deployment/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deployment/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fa1d5938fe52b0e817a",
+    "url": "/react-deployment/static/js/main.9d005db2.chunk.js"
+  },
+  {
+    "revision": "499457d2851a29fbde0a",
+    "url": "/react-deployment/static/js/runtime-main.33eeac8e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deployment/static/media/logo.5d5d9eef.svg"
+  }
+]);
